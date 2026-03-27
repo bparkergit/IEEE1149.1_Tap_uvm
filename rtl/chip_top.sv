bab7f9873af605ce4b73b6a3bc94d355a98b78a3
B Parker <pabr646@gmail.com>
2026-03-27 16:29:06 -0700

    Update chip_top.sv

--- rtl/chip_top.sv
+++ rtl/chip_top.sv
@@ -1,10 +1,11 @@
 module chip_top #(
-    parameter IR_WIDTH    = 4,
-    parameter DR_WIDTH    = 32,  // total DR width for BISR + SIB
-    parameter BISR_WIDTH  = 8,   // BISR data width (excluding SIB)
-    parameter NUM_SIB_BITS = 2,  // number of SIB bits
-    parameter MBIST_WIDTH = 16,
-    parameter IDCODE      = 32'h1234ABCD
+    parameter IR_WIDTH            = 4,
+    parameter BISR_WIDTH          = 8,     // BISR data bits
+    parameter NUM_SEGMENT_BITS    = 2,     // Segment select bits
+    parameter MBIST_WIDTH         = 16,
+    parameter NUM_INSTRUMENTS     = 2,     // BISR + MBIST
+    parameter INSTR_SHIFT_WIDTH   = 32,    // max width of instrument shift register
+    parameter IDCODE              = 32'h1234ABCD
 )(
     input  logic TCK,
     input  logic TMS,
@@ -14,16 +15,16 @@ module chip_top #(
 );
 
     // ---------------------------
-    // TAP controller instance
+    // TAP Controller
     // ---------------------------
     logic [IR_WIDTH-1:0] ir_out;
-    logic [DR_WIDTH-1:0] user_dr_shift;
+    logic [INSTR_SHIFT_WIDTH-1:0] dr_shift;
     logic bypass_enable;
     logic tap_tdo;
 
     tap_controller #(
         .IR_WIDTH(IR_WIDTH),
-        .DR_WIDTH(DR_WIDTH),
+        .DR_WIDTH(INSTR_SHIFT_WIDTH),
         .IDCODE(IDCODE)
     ) tap0 (
         .TCK(TCK),
@@ -32,38 +33,49 @@ module chip_top #(
         .TDI(TDI),
         .TDO(tap_tdo),
         .ir_out(ir_out),
-        .dr_out(user_dr_shift),
+        .dr_out(dr_shift),
         .bypass_enable(bypass_enable)
     );
 
     // ---------------------------
-    // BISR instrument
+    // IJTAG / Instrument Bus
     // ---------------------------
-    logic bisr_tdo;
-    logic [BISR_WIDTH-1:0] bisr_data;
-    logic [NUM_SIB_BITS-1:0] sib_bits;
+    logic [NUM_INSTRUMENTS-1:0][INSTR_SHIFT_WIDTH-1:0] instr_shift;
+    logic ijtag_tdo;
 
-    bisr #(
-        .DATA_WIDTH(BISR_WIDTH),
-        .NUM_SIB_BITS(NUM_SIB_BITS)
-    ) bisr0 (
+    ijtag_bus #(
+        .NUM_INSTR(NUM_INSTRUMENTS),
+        .MAX_WIDTH(INSTR_SHIFT_WIDTH)
+    ) bus0 (
         .tck(TCK),
         .trst_n(TRST),
-        .tdi(tap_tdo),         // TAP drives BISR
-        .tdo(bisr_tdo),
-        .ir(ir_out),
-        .user_dr_shift(user_dr_shift)
+        .tdi_in(tap_tdo),
+        .tdo_out(ijtag_tdo)
     );
 
     // ---------------------------
-    // Dummy MBIST module
+    // Instruments
     // ---------------------------
+    // BISR
+    bisr #(
+        .DATA_WIDTH(BISR_WIDTH),
+        .NUM_SEGMENT_BITS(NUM_SEGMENT_BITS)
+    ) bisr0 (
+        .tck(TCK),
+        .trst_n(TRST),
+        .tdi(ijtag_tdo),
+        .tdo(instr_shift[0][0]),
+        .ir(ir_out),
+        .user_dr_shift(dr_shift[BISR_WIDTH + NUM_SEGMENT_BITS-1:0])
+    );
+
+    // Dummy MBIST
     dummy_mbist #(
         .WIDTH(MBIST_WIDTH)
     ) mbist0 (
         .tck(TCK),
         .trst_n(TRST),
-        .tdi_in(bisr_tdo),
+        .tdi_in(instr_shift[0][0]),
         .tdo_out(TDO)
     );
 
