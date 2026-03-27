06cabafd522b9086810c339ec7104c574c7101a1
B Parker <pabr646@gmail.com>
2026-03-27 16:59:04 -0700

    Update ijtag_bus.sv

--- rtl/ijtag_bus.sv
+++ rtl/ijtag_bus.sv
@@ -5,23 +5,23 @@ module ijtag_bus #(
     input  logic tck,
     input  logic trst_n,
     input  logic tdi_in,
-    output logic tdo_out
+    output logic tdo_out,
+    output logic [NUM_INSTR-1:0][MAX_WIDTH-1:0] instrument_shift_reg
 );
 
-    logic [NUM_INSTR-1:0][MAX_WIDTH-1:0] shift_regs;
     logic tdo_chain;
 
     // simple serial chain through instruments
-    assign tdo_chain = shift_regs[NUM_INSTR-1][0];
-    assign tdo_out = tdo_chain;
+    assign tdo_chain = instrument_shift_reg[NUM_INSTR-1][0];
+    assign tdo_out   = tdo_chain;
 
     always_ff @(posedge tck or negedge trst_n) begin
         if (!trst_n) begin
-            shift_regs <= '{default:'0};
+            instrument_shift_reg <= '{default:'0};
         end else begin
-            shift_regs[0] <= {tdi_in, shift_regs[0][MAX_WIDTH-1:1]};
+            instrument_shift_reg[0] <= {tdi_in, instrument_shift_reg[0][MAX_WIDTH-1:1]};
             for (int i=1; i<NUM_INSTR; i++)
-                shift_regs[i] <= {shift_regs[i-1][0], shift_regs[i][MAX_WIDTH-1:1]};
+                instrument_shift_reg[i] <= {instrument_shift_reg[i-1][0], instrument_shift_reg[i][MAX_WIDTH-1:1]};
         end
     end
 
