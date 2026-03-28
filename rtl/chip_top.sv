37c348ba0175cbf46aad851ba1c11c238a80f455
B Parker <pabr646@gmail.com>
2026-03-27 18:05:44 -0700

    Update chip_top.sv

--- rtl/chip_top.sv
+++ rtl/chip_top.sv
@@ -1,10 +1,4 @@
-module chip_top #(
-    parameter IR_WIDTH          = 4,
-    parameter BISR_WIDTH        = 8,
-    parameter MBIST_WIDTH       = 16,
-    parameter INSTR_SHIFT_WIDTH = 32,
-    parameter IDCODE            = 32'h1234ABCD
-)(
+module chip_top (
     input  logic TCK,
     input  logic TMS,
     input  logic TRST,
@@ -12,48 +6,32 @@ module chip_top #(
     output logic TDO
 );
 
-    // ---------------------------
-    // TAP controller
-    // ---------------------------
-    logic [IR_WIDTH-1:0] ir_out;
-    logic [INSTR_SHIFT_WIDTH-1:0] dr_shift;
+    logic shift_dr, capture_dr, update_dr;
     logic tap_tdo;
 
-    tap_controller #(
-        .IR_WIDTH(IR_WIDTH),
-        .DR_WIDTH(INSTR_SHIFT_WIDTH),
-        .IDCODE(IDCODE)
-    ) tap0 (
+    // TAP controller (must expose DR state signals)
+    tap_controller tap0 (
         .TCK(TCK),
         .TMS(TMS),
         .TRST(TRST),
         .TDI(TDI),
         .TDO(tap_tdo),
-        .ir_out(ir_out),
-        .dr_out(dr_shift)
+
+        .shift_dr(shift_dr),
+        .capture_dr(capture_dr),
+        .update_dr(update_dr)
     );
 
-    // ---------------------------
-    // IJTAG bus (instrument wrapper)
-    // ---------------------------
-    logic bisr_tdo, mbist_tdo;
-
-    ujtag_bus #(
-        .BISR_WIDTH(BISR_WIDTH),
-        .MBIST_WIDTH(MBIST_WIDTH)
-    ) bus0 (
+    // IJTAG network
+    ujtag_bus bus0 (
         .tck(TCK),
         .trst_n(TRST),
-        .tdi_in(tap_tdo),
-        .tdo_bisr(bisr_tdo),
-        .tdo_mbist(mbist_tdo),
-        .ir(ir_out),
-        .dr_shift(dr_shift)
-    );
+        .tdi(tap_tdo),
+        .tdo(TDO),
 
-    // ---------------------------
-    // TDO output from last instrument
-    // ---------------------------
-    assign TDO = mbist_tdo;
+        .shift_dr(shift_dr),
+        .capture_dr(capture_dr),
+        .update_dr(update_dr)
+    );
 
 endmodule
