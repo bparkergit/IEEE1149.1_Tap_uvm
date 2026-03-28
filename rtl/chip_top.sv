59b9114154f64be50892254729458c334c93686d
B Parker <pabr646@gmail.com>
2026-03-27 17:14:35 -0700

    Update chip_top.sv

--- rtl/chip_top.sv
+++ rtl/chip_top.sv
@@ -1,8 +1,8 @@
 module chip_top #(
     parameter IR_WIDTH          = 4,
-    parameter BISR_WIDTH        = 8,     // width of BISR data
-    parameter MBIST_WIDTH       = 16,    // MBIST width
-    parameter INSTR_SHIFT_WIDTH = 32,    // max width for instrument bus shift register
+    parameter BISR_WIDTH        = 8,
+    parameter MBIST_WIDTH       = 16,
+    parameter INSTR_SHIFT_WIDTH = 32,
     parameter IDCODE            = 32'h1234ABCD
 )(
     input  logic TCK,
@@ -13,7 +13,7 @@ module chip_top #(
 );
 
     // ---------------------------
-    // TAP Controller
+    // TAP controller
     // ---------------------------
     logic [IR_WIDTH-1:0] ir_out;
     logic [INSTR_SHIFT_WIDTH-1:0] dr_shift;
@@ -34,45 +34,26 @@ module chip_top #(
     );
 
     // ---------------------------
-    // Separate instrument shift registers
+    // IJTAG bus (instrument wrapper)
     // ---------------------------
-    logic [INSTR_SHIFT_WIDTH-1:0] bisr_shift_reg;
-    logic [INSTR_SHIFT_WIDTH-1:0] mbist_shift_reg;
+    logic bisr_tdo, mbist_tdo;
 
-    // ---------------------------
-    // Single BISR
-    // ---------------------------
-    bisr #(
-        .DATA_WIDTH(BISR_WIDTH)
-    ) bisr0 (
+    ujtag_bus #(
+        .BISR_WIDTH(BISR_WIDTH),
+        .MBIST_WIDTH(MBIST_WIDTH)
+    ) bus0 (
         .tck(TCK),
         .trst_n(TRST),
-        .tdi(tap_tdo),
-        .tdo(bisr_shift_reg[0]), // LSB of BISR shift register
+        .tdi_in(tap_tdo),
+        .tdo_bisr(bisr_tdo),
+        .tdo_mbist(mbist_tdo),
         .ir(ir_out),
-        .user_dr_shift(dr_shift[BISR_WIDTH-1:0])
+        .dr_shift(dr_shift)
     );
 
     // ---------------------------
-    // Dummy MBIST controller
+    // TDO output from last instrument
     // ---------------------------
-    dummy_mbist #(
-        .WIDTH(MBIST_WIDTH)
-    ) mbist0 (
-        .tck(TCK),
-        .trst_n(TRST),
-        .tdi_in(bisr_shift_reg[0]),  // serial input from BISR
-        .tdo_out(mbist_shift_reg[0])  // serial output to TDO
-    );
-
-    // ---------------------------
-    // TDO output comes from last instrument
-    // ---------------------------
-    assign TDO = mbist_shift_reg[0];
-
-    // ---------------------------
-    // Single memory array for BISR
-    // ---------------------------
-    logic [BISR_WIDTH-1:0] memory [0:255]; // one memory block
+    assign TDO = mbist_tdo;
 
 endmodule
