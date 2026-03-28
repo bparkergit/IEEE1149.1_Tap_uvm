1b2b119b04f6addcf0a28aaa881de9cf8d5c8109
B Parker <pabr646@gmail.com>
2026-03-27 17:18:14 -0700

    Update dummy_mbist.sv

--- rtl/dummy_mbist.sv
+++ rtl/dummy_mbist.sv
@@ -3,18 +3,26 @@ module dummy_mbist #(
 )(
     input  logic tck,
     input  logic trst_n,
-    input  logic tdi_in,
-    output logic tdo_out
+    input  logic tdi_in,      // serial input from previous instrument
+    output logic tdo_out,     // serial output to next in chain
+    input  logic enable       // SIB enable / instrument select
 );
 
-    // Just passes input to output (dummy)
+    // ---------------------------
+    // Shift register for MBIST
+    // ---------------------------
     logic [WIDTH-1:0] shift_reg;
 
     always_ff @(posedge tck or negedge trst_n) begin
-        if (!trst_n) shift_reg <= '0;
-        else shift_reg <= {tdi_in, shift_reg[WIDTH-1:1]};
+        if (!trst_n)
+            shift_reg <= '0;
+        else if (enable)
+            shift_reg <= {tdi_in, shift_reg[WIDTH-1:1]};
     end
 
+    // ---------------------------
+    // TDO comes from LSB of shift register
+    // ---------------------------
     assign tdo_out = shift_reg[0];
 
 endmodule
