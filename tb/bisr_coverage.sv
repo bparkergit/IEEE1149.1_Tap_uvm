1a3cc4ad84712e750bd05db84608558a3639d1df
B Parker <pabr646@gmail.com>
2026-03-28 21:15:16 -0700

    Update bisr_coverage.sv

--- tb/bisr_coverage.sv
+++ tb/bisr_coverage.sv
@@ -5,35 +5,28 @@ class bisr_coverage extends uvm_subscriber #(bisr_seq_item);
   `uvm_component_utils(bisr_coverage)
 
        covergroup cg_transaction with function sample(
-    bit       wr_en,
-    bit       rd_en,
-    bit [7:0] wr_data = 8'h00,   // optional, default if not passed
-    bit [7:0] rd_data = 8'h00,
-    bit       full,
-    bit       empty
+    bit       wr_ir,
+    bit       wr_dr,
+    bit [7:0] instr = 8'h00, 
+    bit       rd_dr
   );
-        coverpoint wr_en {
+        coverpoint wr_ir {
           bins low   = {0};
           bins high  = {1};
         }
 
-        coverpoint rd_en {
+        coverpoint wr_dr {
           bins low  = {0};
           bins high  = {1};
         }
         
-        coverpoint full {
-     	 bins full    = {1};
-      	 bins not_full= {0};
+        coverpoint rd_dr {
+          bins low  = {0};
+          bins high = {1};
     	}
 
-    	coverpoint empty {
-      	bins empty    = {1};
-      	bins not_empty= {0};
-    	}
+    	coverpoint instr;
 
-    cross wr_en, full;
-    cross rd_en, empty;
  
       endgroup
         
@@ -47,16 +40,14 @@ class bisr_coverage extends uvm_subscriber #(bisr_seq_item);
       // This is called automatically via analysis_export
   virtual function void write(bisr_seq_item t);
     // debug
-      `uvm_info("COV_SAMPLE", $sformatf("Sampling txn: wr_en=%0b rd_en=%0b full=%0b empty=%0b", 
-                                     t.wr_en, t.rd_en, t.full, t.empty), UVM_MEDIUM)
+    `uvm_info("COV_SAMPLE", $sformatf("Sampling txn: wr_ir=%0b wr_dr=%0b rd_rd=%0b instr=%0b", 
+                                     t.wr_ir, t.wr_dr, t.rd_dr, t.instr), UVM_MEDIUM)
     // Pass relevant fields to the covergroup's sample function
     cg_transaction.sample(
-      .wr_en   (t.wr_en),
-      .rd_en   (t.rd_en),
-      .wr_data (t.wr_data),
-      .rd_data (t.rd_data),
-      .full    (t.full),
-      .empty   (t.empty)
+      .wr_ir   (t.wr_ir),
+      .wr_dr   (t.wr_dr),
+      .rd_dr (t.rd_dr),
+      .instr (t.instr)
     );
   endfunction
 
