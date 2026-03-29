a3022bb5ec8d10b6a94bbe3480cff013af956956
B Parker <pabr646@gmail.com>
2026-03-28 21:07:14 -0700

    Update bisr_monitor.sv

--- tb/bisr_monitor.sv
+++ tb/bisr_monitor.sv
@@ -6,38 +6,87 @@ class bisr_monitor extends uvm_monitor;
         
   uvm_analysis_port #(bisr_seq_item) ap;
          virtual bisr_if.MONITOR vif;
+
+     
+    
+      // TAP states
+    typedef enum bit [3:0] {
+        TEST_LOGIC_RESET, RUN_TEST_IDLE, SELECT_DR_SCAN,
+        CAPTURE_DR, SHIFT_DR, EXIT1_DR, PAUSE_DR, EXIT2_DR, UPDATE_DR,
+        SELECT_IR_SCAN, CAPTURE_IR, SHIFT_IR, EXIT1_IR, PAUSE_IR, EXIT2_IR, UPDATE_IR
+    } tap_state_t;
+
+    tap_state_t state, next_state;
   
-  function new(string name = "bisr_monitor", uvm_component parent);
-          super.new(name,parent);
-        endfunction
+  bit [3:0] instr;
+  bit [31:0] data;
+  
+  
+  function new(string name = "bisr_monitor", uvm_component parent);            
+    super.new(name,parent);       
+  endfunction
+
+            
+  function void build_phase(uvm_phase phase);
+              ap = new("ap",this);
+
+              if (!uvm_config_db#(virtual bisr_if.MONITOR)::get(this,"","vif",vif)) 					begin
+                 `uvm_fatal("NOVIF", "Virtual interface not set")
+                end
         
-        function void build_phase(uvm_phase phase);
-          ap = new("ap",this);
-          
-          if (!uvm_config_db#(virtual bisr_if.MONITOR)::get(this,"","vif",vif)) 			begin
- 		     `uvm_fatal("NOVIF", "Virtual interface not set")
- 			end
-        endfunction
+           
+  endfunction
+  
+
         
-        task run_phase(uvm_phase phase);
+  task run_phase(uvm_phase phase);
           bisr_seq_item txn;
           
           forever begin
             @(vif.cb_mon);
+                 
+          case(state)
+              TEST_LOGIC_RESET: state = TMS ? TEST_LOGIC_RESET : RUN_TEST_IDLE;
+              RUN_TEST_IDLE:    state = TMS ? SELECT_DR_SCAN : RUN_TEST_IDLE;
+              SELECT_DR_SCAN:   state = TMS ? SELECT_IR_SCAN : CAPTURE_DR;
+              CAPTURE_DR:       state = TMS ? EXIT1_DR : SHIFT_DR;
+              SHIFT_DR:         state = TMS ? EXIT1_DR : SHIFT_DR;
+              EXIT1_DR:         state = TMS ? UPDATE_DR : PAUSE_DR;
+              PAUSE_DR:         state = TMS ? EXIT2_DR : PAUSE_DR;
+              EXIT2_DR:         state = TMS ? UPDATE_DR : SHIFT_DR;
+              UPDATE_DR:        state = TMS ? SELECT_DR_SCAN : RUN_TEST_IDLE;
+              SELECT_IR_SCAN:   state = TMS ? TEST_LOGIC_RESET : CAPTURE_IR;
+              CAPTURE_IR:       state = TMS ? EXIT1_IR : SHIFT_IR;
+              SHIFT_IR:         state = TMS ? EXIT1_IR : SHIFT_IR;
+              EXIT1_IR:         state = TMS ? UPDATE_IR : PAUSE_IR;
+              PAUSE_IR:         state = TMS ? EXIT2_IR : PAUSE_IR;
+              EXIT2_IR:         state = TMS ? UPDATE_IR : SHIFT_IR;
+              UPDATE_IR:        state = TMS ? SELECT_DR_SCAN : RUN_TEST_IDLE;
+              default:          state = TEST_LOGIC_RESET;
+          endcase
 
-         	 txn = bisr_seq_item::type_id::create("txn");
-          	 txn.wr_en = vif.cb_mon.wr_en;
-             txn.rd_en = vif.cb_mon.rd_en;
-             txn.wr_data = vif.cb_mon.wr_data;
-             txn.empty = vif.cb_mon.empty;
-             txn.full = vif.cb_mon.full;
-              
-         	 ap.write(txn);
-              
-              `uvm_info("WRITE_MON", $sformatf("Write observed: %0h", txn.wr_data), UVM_LOW);
+         if(vif.cb_mon.TRST)
+                state = TEST_LOGIC_RESET;
+            
+            
+            if(state == SHIFT_IR)
+              instr = {instr[30:0],TDI};
+
+            if(state == EXIT1_IR)
+              begin
+                txn = bisr_seq_item::type_id::create("txn");
+                txn.instr = instr;
+                txn.wr_ir = 1'b1;
+                ap.write(txn);     
+                
+                `uvm_info("MON", $sformatf("Write IR observed: %0h", txn.instr), UVM_LOW);
+              end
 
           end
-        endtask
+        
+  endtask
+  
+  
         
         
         
