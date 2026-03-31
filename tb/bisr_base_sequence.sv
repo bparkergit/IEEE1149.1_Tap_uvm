2f1a26e4985d2e6bb34bc65d68da4800200518da
B Parker <pabr646@gmail.com>
2026-03-30 17:47:29 -0700

    Update bisr_base_sequence.sv

--- tb/bisr_base_sequence.sv
+++ tb/bisr_base_sequence.sv
@@ -22,9 +22,9 @@ class bisr_base_sequence extends uvm_sequence #(bisr_seq_item);
           wr_ir==1'b1;
           wr_dr==1'b0;
           rd_dr==1'b0;
-          instr==4'b0001; });
+          instr==4'b0010; });
         
-        `uvm_info("SEQ", $sformatf("Generated item: instr=%0b data=%02h", item.instr, item.data), UVM_MEDIUM) 
+        `uvm_info("SEQ", $sformatf("Write IR: instr=%4b", item.instr), UVM_MEDIUM) 
         
     	finish_item(item);
       end
@@ -38,7 +38,7 @@ class bisr_base_sequence extends uvm_sequence #(bisr_seq_item);
           rd_dr==1'b0; 
           data==32'hDEADBEEF; });
         
-        `uvm_info("SEQ", $sformatf("Generated item: instr=%0b data=%02h", item.instr, item.data), UVM_MEDIUM) 
+        `uvm_info("SEQ", $sformatf("Write DR: data=%02h", item.instr, item.data), UVM_MEDIUM) 
         
     	finish_item(item);
       end  
@@ -47,11 +47,11 @@ class bisr_base_sequence extends uvm_sequence #(bisr_seq_item);
       begin
   	 	start_item(item); 
         assert(item.randomize() with {
-          wr_dr==1'b1;
+          wr_dr==1'b0;
           wr_ir==1'b0;
           rd_dr==1'b1; });
         
-        `uvm_info("SEQ", $sformatf("Generated item: instr=%0b data=%02h", item.instr, item.data), UVM_MEDIUM) 
+        `uvm_info("SEQ", $sformatf("Read DR: instr=%4b data=%02h", item.instr, item.data), UVM_MEDIUM) 
         
     	finish_item(item);
       end  
