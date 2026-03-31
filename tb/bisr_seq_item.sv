aab9c8a6cee3211c0d3ae537d7a07cb429864f62
B Parker <pabr646@gmail.com>
2026-03-30 20:49:58 -0700

    Update bisr_seq_item.sv

--- tb/bisr_seq_item.sv
+++ tb/bisr_seq_item.sv
@@ -5,7 +5,8 @@ class bisr_seq_item extends uvm_sequence_item;
 
 
   rand bit [3:0]	 instr;
-  rand bit [31:0]	 data;
+  rand bit [31:0]	 data_tdi;
+  rand bit [31:0]	 data_tdo;
   rand bit	wr_ir;
   rand bit	wr_dr;
   rand bit	rd_dr;
