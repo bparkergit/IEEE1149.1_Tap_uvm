dd2ec1da58d4293be5646b63c78f8634055c6468
B Parker <pabr646@gmail.com>
2026-04-02 02:52:12 -0700

    Update bisr_base_sequence.sv

--- tb/bisr_base_sequence.sv
+++ tb/bisr_base_sequence.sv
@@ -5,6 +5,9 @@ class bisr_base_sequence extends uvm_sequence #(bisr_seq_item);
 
   `uvm_object_utils(bisr_base_sequence)
 
+        logic [7:0] saved_addr;
+      
+  
   function new(string name = "bisr_base_sequence");
         super.new(name);
     endfunction
@@ -12,7 +15,7 @@ class bisr_base_sequence extends uvm_sequence #(bisr_seq_item);
     task body();
         bisr_seq_item item;
             item = bisr_seq_item::type_id::create("item");
-      
+
       // start the test
       
       // write IR (SIB)
@@ -37,13 +40,20 @@ class bisr_base_sequence extends uvm_sequence #(bisr_seq_item);
           wr_ir==1'b0;
           rd_dr==1'b0; 
           dr_bits==2; 
-          data_tdi==32'b10; });   // [sib_b], [sib_m]
+          data_tdi[1]==1'b1;  // [sib_b], [sib_m]
+          data_tdi[0]==1'b0;
+          });   
         
         `uvm_info("SEQ", $sformatf("Write DR: data=%0b", item.data_tdi), UVM_MEDIUM) 
         
     	finish_item(item);
       end  
      
+
+      
+      // random writes
+      repeat(8) begin
+      
       // write DR
       begin
   	 	start_item(item); 
@@ -52,8 +62,13 @@ class bisr_base_sequence extends uvm_sequence #(bisr_seq_item);
           wr_ir==1'b0;
           rd_dr==1'b0; 
           dr_bits==18;
-          data_tdi == {1'b1, 8'b00110011, 8'b10101100, 1'b0}; }); // sib_b,addr,data,sib_m
-        
+          data_tdi[17] == 1'b1;
+          data_tdi[0] == 1'b0;
+        })else 
+        `uvm_fatal(get_type_name(), "Randomize failed for DR write")
+          
+          saved_addr = item.data_tdi[16:9];
+              
         `uvm_info("SEQ", $sformatf("Write DR: addr=%h data=%h", item.data_tdi[16:9], item.data_tdi[8:1]), UVM_MEDIUM) 
         
     	finish_item(item);
@@ -67,43 +82,19 @@ class bisr_base_sequence extends uvm_sequence #(bisr_seq_item);
           wr_ir==1'b0;
           rd_dr==1'b1; 
           dr_bits==18;
-          data_tdi == {1'b1, 8'b00110011, 8'b00000000, 1'b0}; });// sib_b,addr,xxxx,sib_m
-        
+          data_tdi[17] == 1'b1;
+          data_tdi[0] == 1'b0;
+          data_tdi[16:9] == saved_addr;
+        })else 
+          `uvm_fatal(get_type_name(), "Randomize failed for DR read")
+          
         `uvm_info("SEQ", $sformatf("Read DR: addr=%h", item.data_tdi[16:9]), UVM_MEDIUM) 
         
     	finish_item(item);
       end 
+        
+      end
       
-      
-      // write DR
-      begin
-  	 	start_item(item); 
-        assert(item.randomize() with {
-          wr_dr==1'b1;
-          wr_ir==1'b0;
-          rd_dr==1'b0; 
-          dr_bits==18;
-          data_tdi == {1'b1, 8'b10100010, 8'b00101100, 1'b0}; }); // sib_b,addr,data,sib_m
-        
-        `uvm_info("SEQ", $sformatf("Write DR: addr=%h data=%h", item.data_tdi[16:9], item.data_tdi[8:1]), UVM_MEDIUM) 
-        
-    	finish_item(item);
-      end  
-      
-      // read DR
-      begin
-  	 	start_item(item); 
-        assert(item.randomize() with {
-          wr_dr==1'b1;
-          wr_ir==1'b0;
-          rd_dr==1'b1; 
-          dr_bits==18;
-          data_tdi == {1'b1, 8'b10100010, 8'b00000000, 1'b0}; });// sib_b,addr,xxxx,sib_m
-        
-        `uvm_info("SEQ", $sformatf("Read DR: addr=%h", item.data_tdi[16:9]), UVM_MEDIUM) 
-        
-    	finish_item(item);
-      end  
       
     endtask
 
