5e8bb61c93150a09e5bbe023b6629fd257435840
B Parker <pabr646@gmail.com>
2026-04-02 01:06:48 -0700

    Update bisr_base_sequence.sv

--- tb/bisr_base_sequence.sv
+++ tb/bisr_base_sequence.sv
@@ -54,7 +54,7 @@ class bisr_base_sequence extends uvm_sequence #(bisr_seq_item);
           dr_bits==18;
           data_tdi == {1'b1, 8'b00110011, 8'b10101100, 1'b0}; }); // sib_b,addr,data,sib_m
         
-         `uvm_info("SEQ", $sformatf("Write DR: data=%b", item.data_tdi[17:0]), UVM_MEDIUM) 
+        `uvm_info("SEQ", $sformatf("Write DR: addr=%h data=%h", item.data_tdi[16:9], item.data_tdi[8:1]), UVM_MEDIUM) 
         
     	finish_item(item);
       end  
@@ -69,7 +69,7 @@ class bisr_base_sequence extends uvm_sequence #(bisr_seq_item);
           dr_bits==18;
           data_tdi == {1'b1, 8'b00110011, 8'b00000000, 1'b0}; });// sib_b,addr,xxxx,sib_m
         
-        `uvm_info("SEQ", $sformatf("Read DR: data_tdi=%b", item.data_tdi[17:0]), UVM_MEDIUM) 
+        `uvm_info("SEQ", $sformatf("Read DR: addr=%h", item.data_tdi[16:9]), UVM_MEDIUM) 
         
     	finish_item(item);
       end 
@@ -85,7 +85,7 @@ class bisr_base_sequence extends uvm_sequence #(bisr_seq_item);
           dr_bits==18;
           data_tdi == {1'b1, 8'b10100010, 8'b00101100, 1'b0}; }); // sib_b,addr,data,sib_m
         
-        `uvm_info("SEQ", $sformatf("Write DR: data=%b", item.data_tdi[17:0]), UVM_MEDIUM) 
+        `uvm_info("SEQ", $sformatf("Write DR: addr=%h data=%h", item.data_tdi[16:9], item.data_tdi[8:1]), UVM_MEDIUM) 
         
     	finish_item(item);
       end  
@@ -100,7 +100,7 @@ class bisr_base_sequence extends uvm_sequence #(bisr_seq_item);
           dr_bits==18;
           data_tdi == {1'b1, 8'b10100010, 8'b00000000, 1'b0}; });// sib_b,addr,xxxx,sib_m
         
-        `uvm_info("SEQ", $sformatf("Read DR: data_tdi=%b", item.data_tdi[17:0]), UVM_MEDIUM) 
+        `uvm_info("SEQ", $sformatf("Read DR: addr=%h", item.data_tdi[16:9]), UVM_MEDIUM) 
         
     	finish_item(item);
       end  
